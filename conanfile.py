348a9aef299c65eaa70823ad1612c116762442b4
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-01-30 20:56:48 +0100

    atk 2.35.1

--- conanfile.py
+++ conanfile.py
@@ -1,53 +1,81 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, Meson, tools
+from conans.errors import ConanInvalidConfiguration
 import os
+import shutil
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    # Remove following lines if the target lib does not use CMake
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    name = "atk"
+    description = "set of accessibility interfaces that are implemented by other toolkits and applications"
+    topics = ("conan", "atk", "accessibility")
+    url = "https://github.com/bincrafters/conan-atk"
+    homepage = "https://www.atk.org/"
+    license = "LGPL-2.1"
+    generators = "pkg_config"
 
     # Options may need to change depending on the packaged library
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        }
 
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
-    requires = (
-        "zlib/1.2.11"
-    )
-
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
+    
+    def build_requirements(self):
+        self.build_requires('meson/0.53.0')
+        if not tools.which('pkg-config'):
+            self.build_requires('pkg-config_installer/0.29.2@bincrafters/stable')
+    
+    def requirements(self):
+        self.requires('glib/2.58.3@bincrafters/stable')
+
+    def system_requirements(self):
+        pass
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def _configure_meson(self):
+        meson = Meson(self)
+        defs = {}
+        defs['introspection'] = 'false'
+        defs['docs'] = 'false'
+        args=[]
+        args.append('--wrap-mode=nofallback')
+        meson.configure(defs=defs, build_folder=self._build_subfolder, source_folder=self._source_subfolder, pkg_config_paths='.', args=args)
+        return meson
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        for package in self.deps_cpp_info.deps:
+            lib_path = self.deps_cpp_info[package].rootpath
+            for dirpath, _, filenames in os.walk(lib_path):
+                for filename in filenames:
+                    if filename.endswith('.pc'):
+                        shutil.copyfile(os.path.join(dirpath, filename), filename)
+                        tools.replace_prefix_in_pc_file(filename, lib_path)
+        meson = self._configure_meson()
+        meson.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
+        meson = self._configure_meson()
+        meson.install()
         # If the CMakeLists.txt has a proper install method, the steps below may be redundant
         # If so, you can just remove the lines below
         include_folder = os.path.join(self._source_subfolder, "include")
@@ -60,3 +88,4 @@ class LibnameConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ['include/atk-1.0']
